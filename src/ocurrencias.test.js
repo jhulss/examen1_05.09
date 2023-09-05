13528201fd4ad180d5304311ddbbb257df569e39
Jhulians Garcia H <jhulians.garcia@ucb.edu.bo>
2023-09-05 10:32:04 -0400

    Tercera prueba, recibe una palabra y la divide

--- src/ocurrencias.test.js
+++ src/ocurrencias.test.js
@@ -5,15 +5,23 @@ describe("Contar ocurrencias", () => {
   
   it("deberia retornar la palabra ingresada", () => {
     const contar = new contadorOcurrencias();
-    expect(contar.verificarOcurrencias("hola")).toEqual("hola");
+    expect(contar.convertirMinuscula("hola")).toEqual("hola");
   });
 
    it("deberia retornar la palabra ya sea en formato TU, Tu, tU en minuscula", () => {
     const contar = new contadorOcurrencias();
-    expect(contar.verificarOcurrencias("HOLA")).toEqual("hola");
+    expect(contar.convertirMinuscula("HOLA")).toEqual("hola");
   });
 
-  
+   it("deberia retornar la palabra dividida", () => {
+    const contar = new contadorOcurrencias();
+    expect(contar.verificarOcurrencias("HOLA")).toEqual("h,o,l,a");
+  });
+
+
+
+
+
 
 
   
