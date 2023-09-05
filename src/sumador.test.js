a0cca180b2389d47404e12a27b14d9f3edc87935
Jhulians Garcia H <jhulians.garcia@ucb.edu.bo>
2023-09-05 09:18:56 -0400

    Commit de prueba

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,4 +4,8 @@ describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
+
+  it("deberia sumar dos numeros", () => {
+    expect(sumar(4, 4)).toEqual(8);
+  });
 });
